a3b9ba9f73f7edf8d4b107691c5f741c1c8c0239
steveyalcin <steven.yalcin@gmail.com>
2020-01-21 23:33:31 -0400

    initial commit

---
+++ src/widgets/CalendarView.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css'; // only needs to be imported once
+
+const CalendarView = () => {
+  // Render the Calendar
+  var today = new Date();
+  var lastWeek = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - 7
+  );
+
+  return (
+    <InfiniteCalendar
+      width={400}
+      height={600}
+      selected={today}
+      disabledDays={[0, 6]}
+      minDate={lastWeek}
+    />
+  );
+};
+
+export default CalendarView;
